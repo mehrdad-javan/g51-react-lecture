bda95cf5adce3747c4eb292a8eae6c11dc71d243
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-11-12 10:02:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  // js functiuons and variables
+
+  const message = "Hello ReactJS!";
+  
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>this is a simple paragraph.</p>
+    </div>
+  );
+};
+
+export default App;
